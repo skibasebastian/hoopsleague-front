ac537e1aa74d777d6d8b6c594b4b5a18d85bd832
Sebastian Skiba <sebastianskiba@MacBook-Pro-Sebastian.local>
2025-02-26 19:19:03 +0100

    Kontynuacja budowy wyglądu strony.

--- src/Pages/Players.js
+++ src/Pages/Players.js
@@ -11,41 +11,43 @@ function Players() {
     }).catch((err) => {
       console.log(err)
     })
-  },[])
+  }, [])
 
   return (
     <div className="form">
 
-      <h1>Lista zawodników:</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Zawodnik</th>
-            <th scope="col">Drużyna</th>
-            <th scope="col">Numer</th>
-            <th scope="col">Pozycja</th>
-            <th scope="col">Wzrost</th>
-            <th scope="col">Waga</th>
-          </tr>
-        </thead>
-        <tbody>
-          {playersData.map((playersData) => (
-            <tr key={playersData.PlayerID}>
-              <td><Link to={`/players/${playersData.PlayerID}`}>
-                {playersData.FirstName} {playersData.LastName}
-              </Link>
-              </td>
-              <td><Link to={`/teams/${playersData.PlayerTeamID}`}> {playersData.TeamName}
-              </Link>
-              </td>
-              <td> {playersData.Jersey} </td>
-              <td> {playersData.Position} </td>
-              <td> {playersData.Height} cm </td>
-              <td> {playersData.Weight} kg</td>
+      <div className='playerInfoCard'>
+        <div className='boldtext'>Lista zawodników</div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Zawodnik</th>
+              <th scope="col">Drużyna</th>
+              <th scope="col">Numer</th>
+              <th scope="col">Pozycja</th>
+              <th scope="col">Wzrost</th>
+              <th scope="col">Waga</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {playersData.map((playersData) => (
+              <tr key={playersData.PlayerID}>
+                <td><Link to={`/players/${playersData.PlayerID}`}>
+                  {playersData.FirstName} {playersData.LastName}
+                </Link>
+                </td>
+                <td><Link to={`/teams/${playersData.PlayerTeamID}`}> {playersData.TeamName}
+                </Link>
+                </td>
+                <td> {playersData.Jersey} </td>
+                <td> {playersData.Position} </td>
+                <td> {playersData.Height} cm </td>
+                <td> {playersData.Weight} kg</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
     </div>
   )
